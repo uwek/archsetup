cb2200b005454f56f308da43e46516c4eea4d291
uwek <Uwe.Kallmeyer@gmail.com>
2020-11-03 19:23:18 +0100

    Update step1.sh

--- step1.sh
+++ step1.sh
@@ -1,6 +1,8 @@
 # curl -L https://is.gd/dVV0jV
 
 # /dev/sda1 - boot
-parted /dev/sda mklabel msdos
-parted /dev/sda mkpart primary ext4 1 100%
-  
+# parted /dev/sda mklabel msdos
+# parted /dev/sda mkpart primary ext4 1 100%
+mkfs.ext4 /dev/sda1
+mount /dev/sda1 /mnt
+
