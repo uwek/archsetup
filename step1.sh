6cb19896bf5f2f142d11b2f9b931412a26abf1b2
uwek <Uwe.Kallmeyer@gmail.com>
2020-11-03 19:43:48 +0100

    Update step1.sh

--- step1.sh
+++ step1.sh
@@ -1,8 +1,24 @@
 # curl -L https://is.gd/dVV0jV
 
+function step1 () {
 ## fdisk /dev/sda
 ## wifi-menu
-# mkfs.ext4 /dev/sda1
-# mount /dev/sda1 /mnt
+mkfs.ext4 /dev/sda1
+mount /dev/sda1 /mnt
+pacstrap /mnt base wireless_tools netctl iw dialog wpa_supplicant vim linux linux-firmware acpi htop grub
+genfstab -U /mnt >> /mnt/etc/fstab
+}
 
-pacstrap /mnt base wireless_tools netctl iw dialog wpa_supplicant vim linux linux-firmware acpi htop
+function step2() {
+  sed -i 's/#de_DE.UTF-8/de-DE.UTF-8/' /etc/locale.gen
+  locale-gen
+  echo LANG=de_DE.UTF-8 > /etc/locale.conf
+  localectl set-keymap de-latin1-nodeadkeys
+  hwclock --systohc
+  ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+  pacman -S grub
+  grub-install --target=i386-pc /dev/sda
+  grub-mkconfig -o /boot/grub/grub.cfg 
+}
+
+step1
