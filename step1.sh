ff92179346ae6e2f2cf26b4ba0f156f763abf38b
uwek <Uwe.Kallmeyer@gmail.com>
2020-11-03 19:52:32 +0100

    Update step1.sh

--- step1.sh
+++ step1.sh
@@ -7,9 +7,11 @@ mkfs.ext4 /dev/sda1
 mount /dev/sda1 /mnt
 pacstrap /mnt base wireless_tools netctl iw dialog wpa_supplicant vim linux linux-firmware acpi htop grub
 genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt
 }
 
 function step2() {
+  echo textonly > /etc/hostname
   sed -i 's/#de_DE.UTF-8/de-DE.UTF-8/' /etc/locale.gen
   locale-gen
   echo LANG=de_DE.UTF-8 > /etc/locale.conf
@@ -21,4 +23,14 @@ function step2() {
   grub-mkconfig -o /boot/grub/grub.cfg 
 }
 
-step1
+step2
+
+# wifi-menu
+# netctl list 
+# netctl enable wlp1s0-WLAN-589717
+# netctl start wlp1s0-WLAN-589717 
+# pacman -S sudo vim openssh wget 
+# useradd -m -G wheel -s /bin/bash user
+# vi /etc/sudoers 
+# passwd user
+# pacman -S base-devel git wget mc git 
